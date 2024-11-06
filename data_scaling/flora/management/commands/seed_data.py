0e3306b3669f9d08da69680e5460dd6ca3266937
remygist <remy.gistelinck@student.ehb.be>
2024-11-06 14:02:24 +0100

    seeding database correctly

---
+++ data_scaling/flora/management/commands/seed_data.py
@@ -0,0 +1,29 @@
+# myapp/management/commands/seed_data.py
+
+from django.core.management.base import BaseCommand
+from faker import Faker
+from flora.models import Plant, Discoverer
+import random
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        fake = Faker()
+        amount = 100
+        discoverers = []
+
+        for _ in range(amount):
+            discoverer = Discoverer.objects.create(
+                first_name = fake.first_name(),
+                last_name = fake.last_name()
+            )
+            discoverers.append(discoverer)
+
+        for _ in range(amount):
+            Plant.objects.create(
+                name = fake.word(),
+                bloom_start=fake.date_this_year(),
+                bloom_end=fake.date_this_year(),
+                planting_season_start=fake.date_this_year(),
+                planting_season_end=fake.date_this_year(),
+                discoverer=random.choice(discoverers),
+            )
